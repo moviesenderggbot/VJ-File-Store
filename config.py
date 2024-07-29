36fbc77c24c154f8433a4bc7e8817857719c82d0
Unique Shadows <149162089+unique-shadows@users.noreply.github.com>
2024-07-30 00:02:48 +0530

    Improved config.py

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
 import re
 import os
 from os import environ
@@ -16,54 +11,37 @@ def is_enabled(value, default):
         return False
     else:
         return default
-      
+
 # Bot Information
 API_ID = int(environ.get("API_ID", "22620068"))
 API_HASH = environ.get("API_HASH", "11e2c113078324f7e36688baa86c3911")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6062527012').split(
-
-Type / to search
-Code
-Pull requests
-Actions
-Projects
-Security
-Insights
-Settings
-Editing config.py in VJ-File-Store
-BreadcrumbsVJ-File-Store
-/
-config.py
-in
-main
-
-Edit
-
-Preview
-)]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "xyz_files_store_bot") # without @
 PORT = environ.get("PORT", "8080")
 
-# Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
-
-# If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://faxadej879:faxadej879@cluster0.ee0xlwn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
 DB_URI = environ.get("DB_URI", "mongodb+srv://faxadej879:faxadej879@cluster0.ee0xlwn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "techvj")
 
+# Clone Info :-
+CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+
+# If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+CLONE_DB_URI = DB_URI
+CDB_NAME = DB_NAME
+
+
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
-AUTO_DELETE_me in M
-# If Auto Delete Mode Is TDELETE_TIrue Then Fill All Required Variable, If False Then Don't Fill.
-AUTO_DELETE = 2400 # Ti # Tiinutes
-TIME = int(environ.get("AUTO_DELETE", 40))
+
+# If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+
 # Channel Information
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002050604893"))
 
@@ -82,6 +60,7 @@ SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
 SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
 
+
 # Website Info:
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
 
@@ -95,14 +74,10 @@ STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
 MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+
 if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
 
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-    
+URL = environ.get("URL", "https://vj-file-store-4pop.onrender.com")
